6ae5a465bd20217169dbc666f984af770251f15e
NguyenHaiLy02 <nguyenhaily1014@gmail.com>
2022-10-05 09:12:39 +0700

    first commit

---
+++ NguyenHaiLy_BTTaiLop_30p/app/src/main/java/com/example/nguyenhaily_bttailop_30p/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.nguyenhaily_bttailop_30p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,pass;
+    CheckBox luumk;
+    Button login;
+    SharedPreferences sharedPreferences;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("dataLogin",MODE_PRIVATE);
+       Anhxa();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = username.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                if(uname.equals("haily")&& password.equals("1234")){
+                    Toast.makeText(MainActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                    if(luumk.isChecked()){
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("taikhoan",uname);
+                        editor.putString("matkhau",password);
+                        editor.putBoolean("checked",true);
+                        editor.commit();
+                    }
+                }else
+                    Toast.makeText(MainActivity.this, "Lỗi đăng nhập", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void Anhxa() {
+        username = (EditText) findViewById(R.id.editText_user);
+        pass = (EditText) findViewById(R.id.editText_pass);
+        luumk = (CheckBox) findViewById(R.id.checkbox);
+        login = (Button) findViewById(R.id.buttonLogin);
+
+    }
+}
